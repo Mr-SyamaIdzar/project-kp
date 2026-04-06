45edb2a7fae8a26dd77d98de0a84984422f9a1af
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2026-04-06 18:11:58 +0700

    trust all proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust all proxies (Cloudflare → Nginx → PHP) agar HTTPS terdeteksi
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
